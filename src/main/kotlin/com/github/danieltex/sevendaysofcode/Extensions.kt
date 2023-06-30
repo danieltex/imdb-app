de7fc29fea36423a4bb9078ad3865ec6395bcb70
Daniel Teixeira dos Santos <danieltex@gmail.com>
2023-06-29 22:28:55 -0300

    Setting up layout #7DaysOfCode 3

--- src/main/kotlin/com/github/danieltex/sevendaysofcode/Extensions.kt
+++ src/main/kotlin/com/github/danieltex/sevendaysofcode/Extensions.kt
@@ -3,25 +3,15 @@ package com.github.danieltex.sevendaysofcode
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import org.jetbrains.skia.Image
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse.BodyHandlers
-import java.time.Duration
+import java.io.BufferedInputStream
+import java.net.URL
 
 
 fun String.loadImageBitmap(): ImageBitmap {
-    val request = HttpRequest.newBuilder()
-        .uri(URI.create(this))
-        .GET()
-        .build()
+    val bytes = BufferedInputStream(
+        URL(this).openStream()
+    ).use { it.readAllBytes() }
 
-    val client = HttpClient.newBuilder()
-        .followRedirects(HttpClient.Redirect.NORMAL)
-        .connectTimeout(Duration.ofSeconds(20))
-        .build()
-    val response = client.send(request, BodyHandlers.ofByteArray())
-
-    return Image.makeFromEncoded(response.body())
+    return Image.makeFromEncoded(bytes)
         .toComposeImageBitmap()
 }
--- src/main/kotlin/com/github/danieltex/sevendaysofcode/Main.kt
+++ src/main/kotlin/com/github/danieltex/sevendaysofcode/Main.kt
@@ -2,30 +2,68 @@ package com.github.danieltex.sevendaysofcode
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+
 @Composable
 @Preview
 fun App() {
-    val shawshankRedemptionPoster = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg"
-    MaterialTheme {
-        Column {
-            Text("The Shawshank Redemption")
-            Image(
-                bitmap = shawshankRedemptionPoster.loadImageBitmap(),
-                contentDescription = "Shawshank Redemption poster",
-                modifier = Modifier.height(200.dp)
-            )
-            Text("Nota: 9.2 - Ano: 1994")
+    val shawshankRedemptionPoster =
+        "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg"
+    val padding = 10.dp
+
+    MaterialTheme(
+        colors = darkColors()
+    ) {
+        Surface {
+            Box(
+                modifier = Modifier.padding(padding).width(200.dp),
+            ) {
+                Column {
+                    Image(
+                        bitmap = shawshankRedemptionPoster.loadImageBitmap(),
+                        contentDescription = "Shawshank Redemption poster",
+                        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(10.dp))
+                    )
+                    Row(
+                        modifier = Modifier.padding(padding).fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                Icons.Filled.Star,
+                                contentDescription = "Star Icon",
+                                tint = Color.Yellow
+                            )
+                            Text(
+                                text = "9.2",
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Text(text = "1994")
+                    }
+                    Text(
+                        text = "The Shawshank Redemption",
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
